eeeb044a995ba141e0b08dd1e80d3ffc824000a3
Robin Nullans <robinnullans@gmail.com>
2026-05-05 11:24:34 +0200

    modulation

---
+++ fast-food-server.erl
@@ -0,0 +1,25 @@
+-module('fast-food-server').
+-export([start/0]).
+
+start() ->
+    io:format("~n=== FAST-FOOD : La cuisine est ouverte ! ===~n"),
+    Pid = spawn('fast-food', client, [self(), []]),
+    loop_serveur(Pid).
+
+loop_serveur(Pid) ->
+    receive
+        {Pid, burger} ->
+            io:format("CUISINE : Burger en préparation... prêt !~n"),
+            loop_serveur(Pid);
+        {Pid, frites} ->
+            io:format("CUISINE : Frites en préparation... prêtes !~n"),
+            loop_serveur(Pid);
+        {Pid, boisson} ->
+            io:format("CUISINE : Boisson en préparation... prête !~n"),
+            loop_serveur(Pid);
+        {Pid, {recap, Liste}} ->
+            io:format("CUISINE : Récapitulatif : ~p~n", [Liste]),
+            loop_serveur(Pid);
+        {Pid, fin} ->
+            io:format("CUISINE : Commande terminée. Bonne dégustation !~n")
+    end.
--- fast-foodclient.erl
+++ fast-foodclient.erl
@@ -1,28 +1,5 @@
--module('fast-food').
--export([start/0, client/2]).
-
-start() ->
-    io:format("~n=== FAST-FOOD : La cuisine est ouverte ! ===~n"),
-    Pid = spawn('fast-food', client, [self(), []]),
-    loop_serveur(Pid).
-
-loop_serveur(Pid) ->
-    receive
-        {Pid, burger} ->
-            io:format("CUISINE : Burger en préparation... prêt !~n"),
-            loop_serveur(Pid);
-        {Pid, frites} ->
-            io:format("CUISINE : Frites en préparation... prêtes !~n"),
-            loop_serveur(Pid);
-        {Pid, boisson} ->
-            io:format("CUISINE : Boisson en préparation... prête !~n"),
-            loop_serveur(Pid);
-        {Pid, {recap, Liste}} ->
-            io:format("CUISINE : Récapitulatif : ~p~n", [Liste]),
-            loop_serveur(Pid);
-        {Pid, fin} ->
-            io:format("CUISINE : Commande terminée. Bonne dégustation !~n")
-    end.
+-module('fast-food-client').
+-export([client/2]).
 
 client(Pid, Commande) ->
     afficher_menu(),
